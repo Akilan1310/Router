db88d7319348bdcc1198da1ac622326997b42a05
Akilan1310 <157205230+Akilan1310@users.noreply.github.com>
2024-01-26 18:55:19 +0530

    Add files via upload

--- new/src/App.js
+++ new/src/App.js
@@ -1,20 +1,30 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
- import Home from "./Home"
-  import About from "./About"
-import Contact from "./Contact"
-import Out from "./Out"
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Counter from './Counter'
+import Out from './Out'
+import Effect from './Effect'
+import Reducer from './Reducer'
+import Callback from './Callback'
 
 
- export default function App() {
-   return (
-     <BrowserRouter>
-       <Routes>
-           <Route path="/" element ={<Out/>}>
-             <Route index element={<Home/>}/>
-             <Route path="about" element={<About/>}/>
-             <Route path="contact" element={<Contact/>}/>
-           </Route>
-       </Routes>
-     </BrowserRouter>
-   )
- }
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Out/>}>
+      <Route index element={<Home/>}/>
+      <Route path="about" element={<About/>}/>
+      <Route path="contact" element={<Contact/>}/>
+      <Route path="counter" element={<Counter/>}/>
+      <Route path="Effect" element={<Effect/>}/>
+      <Route path="Reducer" element={<Reducer/>}/>
+      <Route path="Callback" element={<Callback/>}/>
+    
+    </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
